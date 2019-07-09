69a6bf895b0e0fa6477a71ffbca8ba2429b08d5c
zhao <zhao@yicksolutions.com>
2019-07-09 14:45:14 +0800

    no message

--- fillHtml.js
+++ fillHtml.js
@@ -1,15 +1,16 @@
 const Momnet = require('moment');
 
 function fillHtml(html, data) {
+    html = enSureTemplate(html, data);
     for (var k of Object.keys(data)) {
         var value = data[k];
         var num = 0;
         var count = countKey(k, html);
         while (num < count) {
+            num++;
             if (value instanceof Array) {
                 var targetString = findTargetElement(k, html);
                 if (targetString === null) {
-                    num++;
                     continue;
                 }
                 var forStrs = findForStrs(targetString);
@@ -34,7 +35,6 @@ function fillHtml(html, data) {
             } else {
                 html = setValue(k, value.toString(), html);
             }
-            num++;
         }
     }
     return html;
@@ -63,14 +63,14 @@ function countKey(k, html) {
 }
 
 function findTargetElement(k, target) {
-    var kRegex = new RegExp(k);
+    var kRegex = new RegExp('<' + k);
     //页面中没有这个key
     if (target.match(kRegex) === null) {
         return null
     }
     var kIdx = target.indexOf(target.match(kRegex)[0]);
     var endIdx = target.indexOf(target.match(new RegExp('/' + k))[0], kIdx);
-    return target.substring(kIdx - 1, endIdx + k.length + 2);
+    return target.substring(kIdx, endIdx + k.length + 2);
 }
 
 function findForStrs(html) {
@@ -98,7 +98,50 @@ function fillHtmlArray(html, forStrs, data) {
 
 function findTemplateStr(targetString) {
     var arr = targetString.split(/\n/);
-    return targetString.replace(arr[0], '').replace(arr[arr.length - 1], '').trim();
+    return targetString.replace(arr[0] + '\n', '').replace('\n' + arr[arr.length - 1], '');
+}
+
+function enSureTemplate(html, data) {
+    for (var k of Object.keys(data)) {
+        var value = data[k];
+        var num = 0;
+        var count = countKey(k, html);
+        while (num < count) {
+            var targetString = findTargetElement(k, html);
+            num++
+            if (targetString === null) {
+                continue;
+            }
+            if (targetString.indexOf("if") !== -1) {
+                var ifExpressionStr = findIf(targetString);
+                //获得if表达式的值
+                var result = new Boolean(eval(ifExpressionStr.replace(k, 'data.' + k)));
+                if (result == true) {
+                    if (value instanceof Array) {
+                        //如果是数组元素，那识别的标签会在数组设值时，被替换为空
+                    } else {
+                        //非数组元素，则替换识别标签为空
+                        var arr = targetString.split(/\n/);
+                        html = html.replace(arr[0], '').replace(arr[arr.length - 1], '');
+                    }
+                } else {
+                    html = html.replace(new RegExp(targetString, 'g'), '')
+                }
+            }
+        }
+    }
+    return html;
+}
+
+function findIf(targetString) {
+    var arr = targetString.split(/\n/);
+    var arr2 = arr[0].split('\"');
+    // ["<username if=", "!username", ">\r"]
+    // 获得if里的表达式
+    var ifIdx = arr2.findIndex(item => {
+        return item.indexOf("if") !== -1;
+    });
+    return arr2[ifIdx + 1]
 }
 
 module.exports = fillHtml;
